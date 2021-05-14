b75cbf4d1f9f119b1be56b7aa2f29afd55cbf323
haryati75 <haryati75@gmail.com>
2021-05-14 14:13:10 +0000

    lab07 added Delete and Edit function

--- lab07/index.js
+++ lab07/index.js
@@ -39,11 +39,90 @@ app.get('/pets', async function(req,res){
 })
 
 
+// display Form - Create a new Pet
+app.get('/pet/create', function(req,res) {
+    res.render('create_pets')
+})
+
+app.post('/pet/create', async function(req,res) {
+    let petName = req.body.petName;
+    let petCategory = req.body.petCategory;
+
+    let newPet = {
+        'id': Math.floor(Math.random() * 100000 + 10000),
+        'category': {
+            'id': Math.floor(Math.random() * 1000000 + 100000),
+            'name': petCategory 
+        },
+        'name': petName,
+        'photoURLs': ['n/a'],
+        'tags': [],
+        'status': 'available'
+    }
+
+    // save the newPet object to the Petstore API database
+    let response = await axios.post(baseURL + "/pet", newPet);
+    res.send(response.data);
+})
+
+// display Form - Update an existing Pet and use Redirect
+app.get('/pet/:petID/update', async function(req, res) {
+
+    // 1. Fetch existing pet information from Petstore API database
+    let petID = req.params.petID;
+    let response = await axios.get(baseURL + "/pet/" + petID);
+
+    // 2. Populate the form with the existing information
+    res.render('edit_pets', {
+        'pet': response.data
+    });
+
+})
+
+// update the Pet information
+app.post('/pet/:petID/update', async function(req,res) {
+    // fetch the existing data
+    let response = await axios.get(baseURL + "/pet/" + req.params.petID);
+    let oldPet = response.data;
+
+    // 
+    let newPetName = req.body.petName;
+    let newPetCategory = req.body.petCategory;
+
+    let newPet = {
+        'id': req.params.petID,
+        'category': {
+            'id': oldPet.category.id,
+            'name': newPetCategory 
+        },
+        'name': newPetName,
+        'photoURLs': ['n/a'],
+        'tags': [],
+        'status': 'available'
+    }
+    response = await axios.put(baseURL + "/pet", newPet);
+    // go to the pets URL
+    res.redirect('/pets');
+})
+
+
+// Delete Pet - display confirmation form
+app.get('/pet/:petID/delete', async function(req, res) {
+    let response = await axios.get(baseURL + '/pet/' + req.params.petID);
+    let pet = response.data;
+    res.render('delete_pet', {
+        'pet': pet
+    })
+})
+
+app.post('/pet/:petID/delete', async function(req, res) {
+    let response = await axios.delete(baseURL + '/pet/' + req.params.petID);
+    res.redirect('/pets');
+})
+
+
 // *************************************************************************
 // 3. START SERVER
 app.listen(3000, function() {
     console.log("Server has started")
 })
-
-
-
